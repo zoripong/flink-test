59efe10eb6d0df454777454a33732baa8c3fa376
Yool Han <yr.han@bucketplace.net>
2024-10-16 20:27:08 +0900

    timer 대신 ttl 옵션 사용하도록 수정

--- src/main/kotlin/dedup/ContentViewCountProcessFunction.kt
+++ src/main/kotlin/dedup/ContentViewCountProcessFunction.kt
@@ -2,11 +2,13 @@ package dedup
 
 import dedup.type.ViewEvent
 import dedup.type.ViewEventDedupKey
+import org.apache.flink.api.common.state.StateTtlConfig
 import org.apache.flink.api.common.state.ValueState
 import org.apache.flink.api.common.state.ValueStateDescriptor
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.util.Collector
+import java.time.Duration
 
 //<K> – Type of the key. <I> – Type of the input elements. <O> – Type of the output elements.
 // Key: User 식별자 (userId or sessionId) + Content 식별자 (contentId)
@@ -19,8 +21,17 @@ class ContentViewCountProcessFunction : KeyedProcessFunction<ViewEventDedupKey, 
     private lateinit var lastViewTimeState: ValueState<Long>
 
     override fun open(parameters: Configuration) {
+        // Set up the ValueState with a TTL
+        val stateTtlConfig = StateTtlConfig
+            .newBuilder(DEDUP_TIME)
+            .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
+            .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
+            .build()
+
         lastViewTimeState = runtimeContext.getState(
-            ValueStateDescriptor("lastViewTime", Long::class.java)
+            ValueStateDescriptor("lastViewTime", Long::class.java).apply {
+                this.enableTimeToLive(stateTtlConfig)
+            }
         )
     }
 
@@ -33,7 +44,7 @@ class ContentViewCountProcessFunction : KeyedProcessFunction<ViewEventDedupKey, 
         val lastViewTime = lastViewTimeState.value()
 
         // 이전 조회 시간이 없거나, DEDUP_TIME 시간이 지난 경우 조회수 증가
-        if (lastViewTime == null || currentTime - lastViewTime > DEDUP_TIME) {
+        if (lastViewTime == null || currentTime - lastViewTime > DEDUP_TIME.toMillis()) {
             println("collect: ${event.toJson()}")
 
             // 조회수 증가 로직 (예: 외부 시스템에 조회수 업데이트)
@@ -41,25 +52,13 @@ class ContentViewCountProcessFunction : KeyedProcessFunction<ViewEventDedupKey, 
 
             // 상태 업데이트: 마지막 조회 시간을 현재 시간으로 설정
             lastViewTimeState.update(currentTime)
-
-            // DEDUP_TIME 시간 후에 상태를 제거하는 타이머 설정
-            context.timerService().registerEventTimeTimer(currentTime + DEDUP_TIME)
         } else {
             println("skip: ${event.toJson()}")
         }
     }
 
-    override fun onTimer(
-        timestamp: Long,
-        ctx: KeyedProcessFunction<ViewEventDedupKey, ViewEvent, ViewEvent>.OnTimerContext,
-        out: Collector<ViewEvent>
-    ) {
-        // 타이머가 만료되면 상태를 제거하여 메모리 관리
-        lastViewTimeState.clear()
-    }
-
     companion object {
-//        private val DEDUP_TIME = 24 * 60 * 60 * 1000 // 24h
-        private val DEDUP_TIME = 30 * 1000 // 30 seconds
+//         private val DEDUP_TIME = Duration.ofHours(24) // 24h
+        private val DEDUP_TIME = Duration.ofSeconds(30) // 30 seconds
     }
 }
